b8b8a471252cf486d222cdb4958a92de36b8c1ae
ktyudha <ktyudha@gmail.com>
2025-08-31 10:11:07 +0700

    fix: allow rec payload from relay

--- lorareceive.py
+++ lorareceive.py
@@ -44,15 +44,15 @@ def events(type, parameter):
 
         payload = payload_bytes[2:].decode("utf-8", errors='ignore').strip()
 
-        # Batasi hanya menerima dari relay
-        # if fromAddr != RELAY_ADDRESS:
-        #     print(f"Ignored packet from unknown node {fromAddr}")
-        #     return
+        Batasi hanya menerima dari relay
+        if fromAddr != RELAY_ADDRESS:
+            print(f"Ignored packet from unknown node {fromAddr}")
+            return
         
-        # # Optional: pastikan alamat tujuan adalah gateway
-        # if toAddr != SERVER_ADDRESS:
-        #     print(f"Paket bukan untuk gateway (tujuan: {toAddr})")
-        #     return
+        # Optional: pastikan alamat tujuan adalah gateway
+        if toAddr != SERVER_ADDRESS:
+            print(f"Paket bukan untuk gateway (tujuan: {toAddr})")
+            return
 
         process_payload(fromAddr, toAddr, rssi, snr, payload)
     else:
